d9322d4755d14778744c805389d7b69dd7638149
Harimir <madsfredrikhagen@hotmail.com>
2022-07-12 15:27:11 +0200

    uppdated compiled vertions

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Light lyslenke[] = new Light[8];
+        Light arrayOfLights[] = new Light[8];
         int moves = 0;
         Light first = new Light();
         Light nekst = new Light();
@@ -12,37 +12,37 @@ public class Main {
         first.setRight(nekst);
         nekst.setLeft(first);
 
-        for (int i=0; i<lyslenke.length; i++){
-            lyslenke[i] = nekst;
+        for (int i=0; i<arrayOfLights.length; i++){
+            arrayOfLights[i] = nekst;
             nekst = new Light();
-            nekst.setLeft(lyslenke[i]);
-            lyslenke[i].setRight(nekst);
+            nekst.setLeft(arrayOfLights[i]);
+            arrayOfLights[i].setRight(nekst);
 
-            //System.out.println("light " + i + " is " + lyslenke[i].onOff);
+            //System.out.println("light " + i + " is " + arrayOfLights[i].onOff);
         }
         last.setRight(first);
-        lyslenke[0].setLeft(lyslenke[(lyslenke.length-1)]);
+        arrayOfLights[0].setLeft(arrayOfLights[(arrayOfLights.length-1)]);
 
         // lag en while loop som leser input, skriver ut det nåværende resultat og når alle Lamp i arrayet er true avslutter
-        while (chainOfLightsOn(lyslenke) == false){
+        while (chainOfLightsOn(arrayOfLights) == false){
             Scanner input = new Scanner(System.in);
             int target = (input.nextInt()-1);
 
 
-            if (target > (lyslenke.length-1) || target < 0){
+            if (target > (arrayOfLights.length-1) || target < 0){
                 System.out.println("Invalid value, please try again");
             }else{
-                lyslenke[target].flip();
+                arrayOfLights[target].flip();
 
                 String display = "Lamp numbers: ";
                 String output =  "Lamp light:   ";
                 // gå igjennom hele listen, bygg Stringene display og output ettervært som du går igjennom
-                for (int i=0; i<lyslenke.length; i++){
+                for (int i=0; i<arrayOfLights.length; i++){
 
 
                     display = display + " " + (i + 1);
 
-                    if (lyslenke[i].getOnOff()){
+                    if (arrayOfLights[i].getOnOff()){
                         output = output + " 1";
                     }else {
                         output = output + " 0";
